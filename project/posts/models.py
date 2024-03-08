9b95bdf475bc8c572596771fac81f965992db7e3
2 <kolya321lol654@gmail.com>
2024-03-08 20:01:18 +0400

    Initial commit

--- project/posts/models.py
+++ project/posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+class Article(models.Model):
+    CAT = (
+        ('tank', 'Танк'),
+        ('heal', 'Медик'),
+        ('dd', 'Урон'),
+        ('buyers', 'Торговец'),
+        ('gildemaster', 'Гилдмастер'),
+        ('quest', 'Квестгивер'),
+        ('smith', 'Кузнец'),
+        ('tanner', 'Кожевник'),
+        ('potion', 'Зельевар'),
+        ('spellmaster', 'Мастер заклинаний'),
+    )
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    category = models.CharField(max_length=32, choices=CAT, default='dd')
+    upload = models.FileField(upload_to='uploads/')
--- project/posts/views.py
+++ project/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import Article
+from django.views.generic import ListView
 
-# Create your views here.
+
+class MainPage(ListView):
+    model = Article
+    ordering = 'title'
+    template_name = 'main.html'
+    context_object_name = 'articles'
